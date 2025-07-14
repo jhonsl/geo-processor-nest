f4cb38fe126a798f1ea0339563c13dd48e8fb7b9
Jhon Edinson Salazar Lemus <jhonzallazar@gmail.com>
2025-07-13 21:35:24 -0500

    Add env flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { GeoModule } from './geo/geo.module';
 import { HttpModule } from '@nestjs/axios';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GeoModule, HttpModule, CacheModule.register()],
+  imports: [GeoModule, HttpModule, CacheModule.register(), ConfigModule.forRoot({
+    isGlobal: true,
+  })],
 })
 export class AppModule {}
--- src/geo/geo.service.ts
+++ src/geo/geo.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { CoordinatesDto } from './dto/input.dto';
 import { firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GeoService {
-  constructor(private readonly http: HttpService) {}
+  constructor(private readonly http: HttpService, private configService: ConfigService) {}
 
   async processCoordinates(dto: CoordinatesDto) {
     const response = await firstValueFrom(
-        this.http.post('http://localhost:8000/geo/process/', dto),
+      this.http.post(
+        `${this.configService.get<string>('BACKEND_URL')}/geo/process`,
+        dto,
+      ),
     );
 
     return response.data;
